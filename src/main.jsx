b3e7dc19292cfa2da7acde8e1515581ee1ccf825
AnLaxina <anilovlaxina@gmail.com>
2025-09-15 14:11:36 -0700

    feat(typography.css): add stylesheet for font sizes and family

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 import "./styles/reset.css";
+import "./styles/typography.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
