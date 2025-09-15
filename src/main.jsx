befa41ecf68c34ef7de63451439dbf0d70a34b4c
AnLaxina <anilovlaxina@gmail.com>
2025-09-15 14:16:37 -0700

    feat(colors.css): add proper color scheme

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 
 import "./styles/reset.css";
 import "./styles/typography.css";
+import "./styles/colors.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
