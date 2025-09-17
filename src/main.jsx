3698074d8c815db7d65de8c6534304d19e9fa062
AnLaxina <anilovlaxina@gmail.com>
2025-09-17 13:41:37 -0700

    feat(button.css): add hover & focus states

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import "./styles/reset.css";
 import "./styles/typography.css";
 import "./styles/colors.css";
+import "./styles/spacing.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
