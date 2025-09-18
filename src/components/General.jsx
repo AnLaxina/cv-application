7a5ece61a056b56d67e36033a59ec6cab1997418
AnLaxina <anilovlaxina@gmail.com>
2025-09-18 14:59:26 -0700

    feat(general.css): add proper styling for general information

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -4,24 +4,26 @@ export default function General() {
     return (
         <section className="general-info">
             <h2>General Information</h2>
-            <div className="input-inline">
-                <label htmlFor="name">Name </label>
-                <input id="name" type="text"></input>
-            </div>
+            <div className="inputs">
+                <div className="input-newline">
+                    <label htmlFor="name">Name </label>
+                    <input id="name" type="text"></input>
+                </div>
 
-            <div className="input-inline">
-                <label htmlFor="email">Email </label>
-                <input id="email" type="email"></input>
-            </div>
+                <div className="input-newline">
+                    <label htmlFor="email">Email </label>
+                    <input id="email" type="email"></input>
+                </div>
 
-            <div className="input-inline">
-                <label htmlFor="phoneNumber">Phone Number </label>
-                <input id="phoneNumber" type="tel" maxLength="15"></input>
-            </div>
+                <div className="input-newline">
+                    <label htmlFor="phoneNumber">Phone Number </label>
+                    <input id="phoneNumber" type="tel" maxLength="15"></input>
+                </div>
 
-            <div className="input-inline">
-                <p>Links</p>
-                <Button text="+ Add Link" type="secondary"/>
+                <div className="input-inline">
+                    <p>Links</p>
+                    <Button text="+ Add Link" type="secondary"/>
+                </div>
             </div>
         </section>
     );
