b7dc7413c1c2187b3cdf4368db972163a8ac24d2
AnLaxina <anilovlaxina@gmail.com>
2025-09-17 19:00:02 -0700

    feat(button): add onClick event prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
 import "../styles/button.css";
 
-export default function Button({ text, type }) {
+export default function Button({ text, type, onClick = () => {}}) {
   switch (type) {
     case "link":
-      return <button className="link">{text}</button>;
+      return <button className="link" onClick={onClick}>{text}</button>;
     case "secondary":
-      return <button className="secondary">{text}</button>;
+      return <button className="secondary" onClick={onClick}>{text}</button>;
     default:
-      return <button className="primary">{text}</button>;
+      return <button className="primary" onClick={onClick}>{text}</button>;
   }
 }
