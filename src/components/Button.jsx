8ba92c1d3d40488d3bedeab20b71c8c23e020b61
AnLaxina <anilovlaxina@gmail.com>
2025-09-29 09:00:25 -0700

    feat(button): add types to each button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,14 @@
 import "../styles/button.css";
 
-export default function Button({ text, type, onClick = () => {}}) {
-  switch (type) {
-    case "link":
-      return <button className="link" onClick={onClick}>{text}</button>;
-    case "secondary":
-      return <button className="secondary" onClick={onClick}>{text}</button>;
-    default:
-      return <button className="primary" onClick={onClick}>{text}</button>;
-  }
+export default function Button({text, type, onClick = () => console.log("You clicked this button!")}) {
+    switch (type) {
+        case "link":
+            return <button type="button" className="link" onClick={onClick}>{text}</button>;
+        case "secondary":
+            return <button type="button" className="secondary" onClick={onClick}>{text}</button>;
+        case "submit":
+            return <button type="submit" className="primary" onClick={onClick}>{text}</button>;
+        default:
+            return <button type="button" className="primary" onClick={onClick}>{text}</button>;
+    }
 }
