073a249966acb665a2477d110cf41df268ec1aa1
AnLaxina <anilovlaxina@gmail.com>
2025-09-28 20:33:28 -0700

    feat(experience): add transition for sections

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,18 +4,39 @@ import Education from "./Education.jsx";
 import Experience from "./Experience.jsx";
 
 import "../styles/general.css";
+import {useState} from "react";
 
 function handleSubmission() {
     console.log("You clicked me!");
 }
 
 export default function Form() {
+    const [currentSection, setSection] = useState(0);
+    const sections = [<General/>, <Education/>, <Experience/>];
+
+    const isEnd = currentSection >= sections.length - 1;
+    const isBeginning = currentSection === 0;
+
+    function moveSection(forward = true) {
+        if (!isEnd && forward) {
+            setSection(currentSection + 1);
+        } else if (!forward) {
+            setSection(currentSection - 1);
+        }
+    }
+
     return (
         <aside className="form">
-            <General/>
-            <Education/>
-            <Experience/>
-            <Button text={"Submit"} onClick={handleSubmission}/>
+            {sections[currentSection]}
+
+            <div className="submission-buttons">
+                {!isBeginning && <Button text="Back" onClick={() => moveSection(false)}/>}
+
+                {!isEnd && <Button text="Next" onClick={moveSection}/>}
+
+                {isEnd && <Button text={"Submit"} onClick={handleSubmission}/>}
+            </div>
+
         </aside>
     )
 }
