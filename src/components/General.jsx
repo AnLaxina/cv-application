55482fae130cc4cccced24c89f122f882559fea2
AnLaxina <anilovlaxina@gmail.com>
2025-09-29 20:30:30 -0700

    feat(general): make inputs required

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,27 +1,28 @@
 import Button from "./Button.jsx";
 
-export default function General({submitMethod}) {
+export default function General({submitMethod = () => console.log("chicken")}) {
     return (
         <section className="general-info">
             <h2>General Information</h2>
-            <form className="inputs" method="post" onSubmit={submitMethod}>
+            <form className="inputs" onSubmit={submitMethod}>
                 <div className="input-newline">
                     <label htmlFor="name">Name </label>
-                    <input id="name" type="text"></input>
+                    <input id="name" type="text" required={true} placeholder="chicken"></input>
                 </div>
 
                 <div className="input-newline">
                     <label htmlFor="email">Email </label>
-                    <input id="email" type="email"></input>
+                    <input id="email" type="email" required={true} placeholder="chicken@gmail.com"></input>
                 </div>
 
                 <div className="input-newline">
                     <label htmlFor="phoneNumber">Phone Number </label>
-                    <input id="phoneNumber" type="tel" maxLength="15"></input>
+                    <input id="phoneNumber" type="tel" maxLength="15" required={true}
+                           placeholder="123-456-7890"></input>
                 </div>
 
                 <div className="input-inline">
-                    <Button text="Next" type="submit" onSubmit={() => console.log("chicken")}/>
+                    <Button text="Next" type="submit"/>
                 </div>
             </form>
         </section>
