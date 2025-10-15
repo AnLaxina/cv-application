985f8334ceaf45ae24b5f8efd127902b4d303399
AnLaxina <anilovlaxina@gmail.com>
2025-10-15 14:42:49 -0700

    feat(preview): add proper styling to preview page

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,3 +1,5 @@
+import "../styles/preview.css";
+
 export default function Preview({data}) {
     function addSection(sectionName) {
         const entries = Object.entries(data[sectionName]);
@@ -10,8 +12,8 @@ export default function Preview({data}) {
 
     function sectionLayout(fieldName, fieldValue) {
         return (
-            <div key={fieldName}>
-                <p>{fieldName}</p>
+            <div className="preview-data" key={fieldName}>
+                <p id="field-name">{fieldName}</p>
                 <p>{fieldValue}</p>
             </div>
 
@@ -20,14 +22,21 @@ export default function Preview({data}) {
 
     return (
         <section className="general-info">
-            <h2>General</h2>
-            {addSection("General")}
+            <h2>Preview & Summary</h2>
+            <div className="section-container">
+                <h2>General</h2>
+                {addSection("General")}
+            </div>
 
-            <h2>Education</h2>
-            {addSection("Education")}
+            <div className="section-container">
+                <h2>Education</h2>
+                {addSection("Education")}
+            </div>
 
-            <h2>Experience</h2>
-            {addSection("Experience")}
+            <div className="section-container">
+                <h2>Experience</h2>
+                {addSection("Experience")}
+            </div>
         </section>
     );
 }
